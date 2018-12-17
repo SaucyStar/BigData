30ab5e53125cabd6914b4ed8035dc4e0d89ae322
SaucyStar <shenjiani921012@gmail.com>
2018-12-16 22:25:37 -0500

    Add files via upload

---
+++ exercise1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+double average(double x, double y);
+int main()
+{
+	using namespace std;
+	double num1,num2;
+	double ave;
+	cout << "Please enter two numbers: ";
+
+	while (!(cin >> num1 >> num2) || (num1 && num2))
+	{
+	   if (num1 && num2)
+		{
+			ave = average(num1, num2);
+			cout << "The average of two non-zero numbers is " << ave << endl;
+			cout << "Please enter two numbers: ";
+		}
+	   	
+	   	else
+		{
+			cin.clear();
+			while (cin.get() != '\n')
+				continue;
+			cout << "Please enter two numbers rightly: ";	
+		} 
+		
+
+	} 
+
+	cout << "You have entered at least one zero number! Bye!\n";   
+
+	return 0;
+}
+
+double average(double x, double y)
+{
+	double temp;
+	temp= 2.0*x*y/(x+y);
+	return temp;
+}
